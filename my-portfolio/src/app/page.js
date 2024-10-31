1a995d5577eff1440f341aa47eabd7604444a82c
BrishaP <152220142+BrishaP@users.noreply.github.com>
2024-10-31 21:51:44 +0000

    h1 placeholder

---
+++ my-portfolio/src/app/page.js
@@ -0,0 +1,10 @@
+import Image from "next/image";
+import styles from "./page.module.css";
+
+export default function Home() {
+  return (
+    <div>
+      <h1>&lt;h1&gt;ME: BRISHA PATEL&lt;/h1&gt;</h1>
+    </div>
+  );
+}
