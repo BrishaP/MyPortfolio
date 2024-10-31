a920ce6e6be0001f5e0216cfe1eb7e0ba5efc3a0
BrishaP <152220142+BrishaP@users.noreply.github.com>
2024-10-31 23:55:02 +0000

    placeholder for about me section

--- my-portfolio/src/app/page.js
+++ my-portfolio/src/app/page.js
@@ -11,10 +11,21 @@ export default function Home() {
           <li><a href='#'>ABOUT</a></li>
           <li><a href='#'>PROJECTS</a></li>
           <li><a href='#'>CONTACT</a></li>
+          <li><a href='#'>CV</a></li>
         </ul>
-        <h2>&lt;nav&gt;</h2>
+        <h2>&lt;/nav&gt;</h2>
       </nav>
       <h1>&lt;h1&gt;ME: BRISHA PATEL&lt;/h1&gt;</h1>
+      <h2>&lt;h2&gt;ABOUT&lt;/h2&gt;</h2>
+      <p>&lt;p&gt;Junior Software Engineer with a background in biomedical science. My interest in tech grew from seeing how advancements in technology can revolutionise and improve lives. I transitioned into tech to create solutions that make a difference. I love tackling problems, have a keen eye for detail, and I’m dedicated to continuous learning.&lt;/p&gt;</p>
+      <h2>&lt;h2&gt;PROJECTS&lt;/h2&gt;</h2>
+      <ul>
+      <h2>&lt;ul&gt;</h2>
+        <li><h3>PantryPal</h3> Image Placeholder<a href='#'>github link</a><a href='#'>Deployed link</a><a href='#'>Learn More</a></li>
+        <li><h3>WebScraperV2</h3> Image Placeholder<a href='#'>github link</a><a href='#'>Deployed link</a><a href='#'>Learn More</a></li>
+        <li><h3>WeatherApp</h3> Image Placeholder<a href='#'>github link</a><a href='#'>Deployed link</a><a href='#'>Learn More</a></li>
+        <h2>&lt;/ul&gt;</h2>      
+      </ul>
     </div>
   );
 }
