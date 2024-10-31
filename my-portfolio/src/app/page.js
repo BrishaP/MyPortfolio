4f662c9a0aeb90ed29dac270ae04456432921f61
BrishaP <152220142+BrishaP@users.noreply.github.com>
2024-10-31 22:30:17 +0000

    adding nav placeholders and removed NEXT CSS

--- my-portfolio/src/app/page.js
+++ my-portfolio/src/app/page.js
@@ -1,9 +1,19 @@
 import Image from "next/image";
-import styles from "./page.module.css";
+// import styles from "./page.module.css";
 
 export default function Home() {
   return (
     <div>
+      <nav>
+        <h2>&lt;nav&gt;</h2>
+        <ul>
+          <li><a href='#'>HOME</a></li>
+          <li><a href='#'>ABOUT</a></li>
+          <li><a href='#'>PROJECTS</a></li>
+          <li><a href='#'>CONTACT</a></li>
+        </ul>
+        <h2>&lt;nav&gt;</h2>
+      </nav>
       <h1>&lt;h1&gt;ME: BRISHA PATEL&lt;/h1&gt;</h1>
     </div>
   );
